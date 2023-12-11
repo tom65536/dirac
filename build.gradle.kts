b91dfa652f25bc0b9353aef7757eed1d97d7a2c9
tom65536 <peregrinus@planet.ms>
2023-12-11 10:09:20 +0100

    refactor(repo): use asciidoc instead of markdown

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,17 @@
 
 plugins {
-    id("hep.dirac.java-application-conventions")
+  id("hep.dirac.java-application-conventions")
 
-    id("gradle.site") version "0.6"
+  id("com.stehno.gradle.site") version "0.0.3"
+  id("com.stehno.gradle.webpreview") version "0.3.0"
 }
+
+group = "hep.dirac"
+
+val ghOwner = "tom65536"
+
+val webUrl = "https://${ghOwner}.github.io/${project.name}/"
+val githubUrl = "https://github.com/$ghOwner}/${project.name}.git"
 
 dependencies {
     implementation("org.apache.commons:commons-text")
@@ -20,5 +28,22 @@ tasks.compileJava {
 }
 
 application {
-    mainClass.set("hep.dirac.app.App")
+  mainClass.set("hep.dirac.app.App")
 }
+
+site {
+  siteUrl = webUrl
+}
+
+webPreview {
+  port = 0
+  copyUrl = false
+  resourceDir = file(layout.buildDirectory.dir("site"))
+  contextPath = "/"
+}
+
+tasks.site.dependsOn = [
+  // 'jacocoTestReport',
+  // 'asciidoctor',
+  'javadoc',
+  'build']
